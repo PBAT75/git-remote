4f328a43632d4078028b48d81a094b923eccf6d8
Patricia Batjom <patriciabatjom@gmail.com>
2018-09-10 16:36:09 +0200

    ajout de var

---
+++ var.php
@@ -0,0 +1,11 @@
+<?php
+$movieName = 'The Last Crusade';
+echo $movieName, '<br>' ;
+$seen = 'yes';
+echo $seen , '<br>';
+
+$releaseYear = '1989';
+echo $releaseYear,'<br>' ;
+
+$movieNote = '8.3';
+echo $movieNote ;
